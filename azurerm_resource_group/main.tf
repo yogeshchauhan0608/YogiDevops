77effa073e8749260307fa7d2d8f5bf903e352f3
Yogesh Chauhan <yogeshchauhan0608@gmail.com>
2026-08-09 01:53:21 +0530

    format terraform configuration

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "yogirg" {
-    for_each =var.resource_group
-    name = each.value.name
-    location = each.value.location
-    
+  for_each = var.resource_group
+  name     = each.value.name
+  location = each.value.location
+
 
 }
 
