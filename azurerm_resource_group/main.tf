a0016012747567fc499b99709e16fffef319fed0
yogeshchauhan0608 <yogeshchauhan0608@gmail.com>
2026-08-19 02:23:30 +0530

    Update main.tf

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,1 +1,26 @@
+resource "azurerm_resource_group" "yogirg" {
+  for_each = var.resource_group
+  name     = each.value.name
+  location = each.value.location
 
+
+}
+
+
+# resource "azurerm_resource_group" "yogirg12" {
+#     for_each =var.resource_group
+#     name = each.value.name
+#     location = each.value.location
+
+
+
+# resource "azurerm_resource_group" "yogirg13" {
+#     for_each =var.resource_group
+#     name = each.value.name
+#     location = each.value.location
+
+
+# resource "azurerm_resource_group" "yogirg15" {
+#     for_each =var.resource_group
+#     name = each.value.name
+#     location = each.value.location
